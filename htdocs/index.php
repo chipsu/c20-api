02bb58684cabb69ec863bc51633b28e04cee4d30
chipsu <chips@metrica.se>
2020-01-20 22:14:50 +0100

    Auth base

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,18 +2,28 @@
 
 namespace c20\api;
 
+# TODO: Move to .env
 define('C20_DB_DSN', 'sqlite:/tmp/c20.db');
+define('C20_JWT_SECRET', 'secret');
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../../php-core/vendor/autoload.php';
 
 use metrica\core\Bootstrap;
+use Ahc\Jwt\JWT;
+
+$modules = [
+  'authModule' => AuthModule::class,
+  'userModule' => UserModule::class,
+  'contentModule' => ContentModule::class,
+];
 
 $di = Bootstrap::di([
   'db' => DB::class,
-  'userModule' => UserModule::class,
-  'contentModule' => ContentModule::class,
-]);
+  'jwt' => function() {
+    return new JWT(C20_JWT_SECRET);
+  },
+] + $modules);
 
 $di->get('router')->get('/', function (DB $db) {
   echo '<pre>';
@@ -27,6 +37,8 @@ $di->get('router')->get('/', function (DB $db) {
   ];
 });
 
-$di->get('userModule')->init();
+foreach(array_keys($modules) as $module) {
+  $di->get($module)->init();
+}
 
 $di->get('app')->run();
